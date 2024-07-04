41add269eb4d561d83be1c165f950939a382a58c
kk <112804222+kar-ik@users.noreply.github.com>
2024-07-04 17:41:06 +0530

    Update dictionary_attack.py

--- src/dictionary_attack.py
+++ src/dictionary_attack.py
@@ -10,23 +10,19 @@ def dictionary_attack(password, dictionary_file):
 if __name__ == "__main__":
     dictionaries = ["indianpass.txt", "uspass.txt", "30000000pass.txt"]
     
-    # Display the available dictionaries
     print("Available dictionaries:")
     for idx, dictionary in enumerate(dictionaries, 1):
         print(f"{idx}. {dictionary}")
     
-    # Prompt the user to select a dictionary file
-    choice = int(input("Select a dictionary file by entering the corresponding number: "))
+        choice = int(input("Select a dictionary file by entering the corresponding number: "))
     if 1 <= choice <= len(dictionaries):
         selected_dictionary = dictionaries[choice - 1]
     else:
         print("Invalid choice. Exiting.")
         exit(1)
     
-    # Prompt the user to enter the password to test
     password = input("Enter the password to test: ")
-    
-    # Perform the dictionary attack
+   
     result = dictionary_attack(password, selected_dictionary)
     if result:
         print(f"Password found: {result}")
